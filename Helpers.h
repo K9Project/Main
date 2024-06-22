e14ea5072c881e7666e920f9b3c43f458b3ce080
K9Project <v.kh1992@mail.ru>
2024-06-23 02:42:47 +0400

    Add project files.

---
+++ Helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+int summ(int a, int b)
+
+{
+
+    return (a + b) * (a + b); 
+
+}
---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int result = summ(1, 3);
+    std::cout << "Result = " << result << std::endl;
+}
+
